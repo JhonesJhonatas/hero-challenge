09fd86caa7b36b3a9c60cbe8f22e7c791020cd7a
Jhones Jhonatas <jhones.jhonatas@outlook.com.br>
2024-02-09 11:21:40 -0300

    makeFunctions :art:

--- src/index.js
+++ src/index.js
@@ -1,32 +1,35 @@
 const name = 'Iron Man'
-const experience = 9002
-let level = ''
+const experience = 1002
 
-if(experience < 1000){
-  level = 'Ferro'
+function getLevelByExperience(experience){
+  if(experience < 1000){
+    return 'Ferro'
+  }
+  
+  if(experience > 1001 && experience < 2000){
+    return 'Bronze'
+  }
+  if(experience > 2001 && experience < 5000 ){
+    return 'Prata'
+  }
+  if(experience > 5001  && experience < 7000 ){
+    return 'Ouro'
+  }
+  if(experience > 7001  && experience < 8000 ){
+    return 'Platina'
+  }
+  if(experience > 8001  && experience < 9000 ){
+    return 'Ascendente'
+  }
+  if(experience > 9001   && experience < 10000 ){
+    return 'Imortal'
+  }
+  if(experience >= 10001 ){
+    return 'Radiante'
+  }
 }
 
-if(experience > 1001 && experience < 2000){
-  level = 'Bronze'
-}
-if(experience > 2001 && experience < 5000 ){
-  level = 'Prata'
-}
-if(experience > 5001  && experience < 7000 ){
-  level = 'Ouro'
-}
-if(experience > 7001  && experience < 8000 ){
-  level = 'Platina'
-}
-if(experience > 8001  && experience < 9000 ){
-  level = 'Ascendente'
-}
-if(experience > 9001   && experience < 10000 ){
-  level = 'Imortal'
-}
-if(experience >= 10001 ){
-  level = 'Radiante'
-}
+const level = getLevelByExperience(experience)
 
 
 console.log(`O Herói de nome ${name} está no nível de ${level}`)
